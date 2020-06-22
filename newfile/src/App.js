7c8fedb8051a18f2a782e7c259ad9d214a79ba33
kaleyfawcett <kaley.fawcett@gmail.com>
2020-06-21 22:21:54 -0400

    almost got toggle to work

--- newfile/src/App.js
+++ newfile/src/App.js
@@ -3,19 +3,49 @@ import './App.css'
 import data from './books.json'
 
 class App extends React.Component {
+  constructor (props) {
+    super(props)
+    this.toggleContent = this.toggleContent.bind(this)
+    this.state = {
+      showContent: false
+    }
+  }
+
+  toggleContent (event) {
+    event.preventDefault()
+    this.setState({
+      showContent: false
+    })
+  }
+
   render () {
+    const { showContent } = this.state
     return (
       <div className='App'>
-        <div className='card'>
-          {data.map((bookDetail) => <div key={data.id}>
-            <h2 className='card-header'>{bookDetail.title}</h2>
-            <p className='card-body'>{bookDetail.author}</p>
-            <p className='card-body'>{bookDetail.shortDescription}</p>
-            <img src={bookDetail.coverImageUrl} className='card-body' />
-          </div>)}
-        </div>
-      </div>
+        {data.map((bookDetail, index) => <div key={data.id} className='container'>
+          <div className='content'>
+            <div className='contentBox'>
+              <div>
+                <h1 className='title'>{bookDetail.title}</h1>
+                <p className='author'>{bookDetail.author}</p>
+                <p className='description'>{bookDetail.shortDescription}</p>
+                <div className='dropdownBox'>
+                  {showContent === true ? <p>{bookDetail.dropdownBox}</p> : ''}
+                  <button handleclick={this.toggleContent}>More Information</button>
+                  <button handleclick={this.onhandleRemoveContentButton}>Less Information</button>
+                  <p className='container_text'> <strong>URL: </strong>{bookDetail.url}</p>
+                  <p className='container_text'><strong>Publisher: </strong>{bookDetail.publisher}</p>
+                  <p className='container_text'><strong>Publication Date: </strong>{bookDetail.publicationDate}</p>
+                  <p className='container_text'><strong>Full Description: </strong>{bookDetail.detailedDescription}</p>
+                </div>
+              </div>
+              <img src={bookDetail.coverImageUrl} className='container_image' />
+            </div>
 
+          </div>
+
+        </div>)}
+      </div>
     )
   }
 }
